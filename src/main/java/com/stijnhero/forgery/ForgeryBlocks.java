9f31d1b54814e00d5ce932fb9ea7e30b195e4ac1
Ruud Gerrits <rgerrits@emixion.nl>
2015-04-29 16:22:09 +0200

    Merge conflict

--- src/main/java/com/stijnhero/forgery/ForgeryBlocks.java
+++ src/main/java/com/stijnhero/forgery/ForgeryBlocks.java
@@ -3,15 +3,10 @@ package com.stijnhero.forgery;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-<<<<<<< HEAD
 import com.stijnhero.forgery.common.block.BlockForgeryDynamicLiquid;
-import com.stijnhero.forgery.common.block.BlockFurnace;
-=======
 import com.stijnhero.forgery.common.block.BlockForgeryFurnace;
->>>>>>> Ruud
 import com.stijnhero.forgery.common.block.BlockHeater;
 import com.stijnhero.forgery.common.block.BlockOre;
 import com.stijnhero.forgery.common.tileentity.TileEntityForgeryFurnace;
@@ -22,13 +17,9 @@ public class ForgeryBlocks {
 	public static Block OreCopper;
 	public static Block OreTin;
 	public static Block ClayHeater;
-<<<<<<< HEAD
-	public static Block Furnace;
 	public static Block BronzeHeater;
 	public static Block LiquidCopper;
-=======
 	public static Block ForgeryFurnace;
->>>>>>> Ruud
 	
 	public static boolean HarvestWood = true;
 	public static boolean LeavesDrop = true;
@@ -41,14 +32,9 @@ public class ForgeryBlocks {
 		LiquidCopper = new BlockForgeryDynamicLiquid(Material.lava);
 		OreCopper = new BlockOre(Material.rock, 1).setHardness(3.0F).setUnlocalizedName("orecopper");
 		OreTin = new BlockOre(Material.rock, 1).setHardness(3.0F).setUnlocalizedName("oretin");
-<<<<<<< HEAD
 		ClayHeater = new BlockHeater(Material.rock, 100, 100).setHardness(3.0F).setUnlocalizedName("clayheater");
-		Furnace = new BlockFurnace(Material.rock).setHardness(3.0F).setUnlocalizedName("furnace");
 		BronzeHeater = new BlockHeater(Material.rock, 500, 500).setHardness(3.0F).setUnlocalizedName("bronzeheater");
-=======
-		ClayHeater = new BlockHeater(Material.rock, 100).setHardness(3.0F).setUnlocalizedName("clayheater");
 		ForgeryFurnace = new BlockForgeryFurnace(Material.rock).setHardness(3.0F).setUnlocalizedName("forgery_furnace");
->>>>>>> Ruud
 	}
 	
 	public static void RegisterBlocksInPre(){
